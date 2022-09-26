8580c788da2e0250530b0fcc49dcce1b4e433d0d
gl003134 <gl003134@gmail.com>
2022-09-26 10:09:35 +0900

    feat : 프로젝트 구성 파일 추가

---
+++ src/main/java/com/example/demo/InCardApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InCardApplication.class, args);
+	}
+
+}
---
+++ src/test/TestWebController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/test")
+public class TestWebController {
+	@GetMapping("/index")
+	public String index() {
+		return "index";
+	}
+}
